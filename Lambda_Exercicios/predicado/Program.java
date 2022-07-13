8814b4f81f3ca2eb0968a2eb79dc3eadfcec882e
thisiszizis <isisgf@ciandt.com>
2022-07-13 10:08:21 -0300

    Commit task 0 e 1

--- Lambda_Exercicios/predicado/Program.java
+++ Lambda_Exercicios/predicado/Program.java
@@ -7,6 +7,7 @@ public class Program {
     public static void main(String[] args) {
         final var localeBrasil = new Locale("pt", "BR");
         Locale.setDefault(localeBrasil);
+        Locale.setDefault(new Locale("pt-BR"));
         Produto produto1 = new Produto("Cafe Chocolate Trufado Baggio",
                 21.99, 250, 120, TiposProduto.COZINHA);
 
