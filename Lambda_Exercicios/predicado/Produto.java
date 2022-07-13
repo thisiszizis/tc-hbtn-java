61596ba81e7d4fd204045d4bca6a2b8c015107c7
thisiszizis <isisgf@ciandt.com>
2022-07-13 09:47:56 -0300

    Commit task 0 e 1

--- Lambda_Exercicios/predicado/Produto.java
+++ Lambda_Exercicios/predicado/Produto.java
@@ -36,6 +36,6 @@ public class Produto {
     
     @Override
     public String toString() {
-        return String.format("%s %,6d %,6d %d %s", nome, preco, peso, quantidadeEmEstoque, tipo);
+        return String.format("%s %,6f %,6f %d %s", nome, preco, peso, quantidadeEmEstoque, tipo);
     }
 }
